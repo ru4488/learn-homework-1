a45f3d53655996357d8419246da85ff1c8fb84ba
Dormidontov Dmitry <44_88@mail.ru>
2020-11-23 22:32:09 +0300

    решил IF'

--- 2_if2.py
+++ 2_if2.py
@@ -14,13 +14,33 @@
   и выводя на экран результаты
 
 """
+def word(old_word1, old_word2):
+    old_word1 = input("первое слов: ")
+    old_word2 = input("второе слово: ")
+    return old_word1, old_word2
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
-if __name__ == "__main__":
-    main()
+def answer(word1, word2):
+    if word2 == 'learn' and len(word1) < len(word2) and len(word1) != len(word2) :
+        return 3
+    elif len(word1) > len(word2):
+        return 2
+    elif word1 == word2:
+        return 1
+    elif type(word1) is str and type(word2) is str:
+        return 0
+
+
+old_word1 = ''
+old_word2 = ''
+word1, word2 = word(old_word1, old_word2)
+print(answer(word1, word2))
+
+
+word1, word2 = word(old_word1, old_word2)
+print(answer(word1, word2))
+
+word1, word2 = word(old_word1, old_word2)
+print(answer(word1, word2))
+
+word1, word2 = word(old_word1, old_word2)
+print(answer(word1, word2))
